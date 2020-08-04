e19dffca74158841396c597e7e670fdcd1417105
Bensnuf <68095081+Bensnuf@users.noreply.github.com>
2020-08-04 14:30:01 -0500

    first attempt at dynamic navbar

--- js/app.js
+++ js/app.js
@@ -15,16 +15,29 @@
 
 /**
  * Define Global Variables
- * 
+ * A Global variable is one that is defined outside a function, all scripts
+ * and fucntions on the webpage can access it. I assume (probably incorrectly)
+ * that this will mostly be used for 'querySelector' and not too much else
+ * at this stange of development for me.
 */
+const navtrial = document.querySelector('ul');
+const navclass = document.getElementsByClassName(".navbar__menu");
+// const navlist = document.getElementsByClassName("navbar__menu").getElementById("navbar__list")
+const dynamic = document.querySelectorAll(".navbar__menu");
+const scroll = document.getElementById('section1');
+const scroll2 = document.getElementById('section2');
+const scroll3 = document.getElementById('section3');
 
+const load = document.querySelector('div');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
+// document.addEventListener("click", function(){ 
+    // alert("Hello World!"); 
+// });
 
 
 /**
@@ -34,6 +47,29 @@
 */
 
 // build the nav
+
+const navtoolbar = document.createElement('li');
+navtoolbar.innerHTML = "Section 1";
+navtrial.appendChild(navtoolbar);
+navtoolbar.addEventListener('click', function(){
+    scroll.scrollIntoView();
+})
+
+
+const navtoolbar2 = document.createElement('li');
+navtoolbar2.innerHTML = "Section 2";
+navtrial.appendChild(navtoolbar2);
+navtoolbar2.addEventListener('click', function(){
+    scroll2.scrollIntoView();
+})
+
+const navtoolbar3 = document.createElement('li');
+navtoolbar3.innerHTML = "Section 3";
+navtrial.appendChild(navtoolbar3);
+navtoolbar3.addEventListener('click', function(){
+    scroll3.scrollIntoView();
+})
+
 
 
 // Add class 'active' to section when near top of viewport

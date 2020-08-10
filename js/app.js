aeda838f3ea3271c3d911ad6ea3b9b91aa496b2f
Bensnuf <68095081+Bensnuf@users.noreply.github.com>
2020-08-10 14:05:37 -0500

    click and active work, cleaned up file as well

--- js/app.js
+++ js/app.js
@@ -16,9 +16,8 @@
 /**
  * Define Global Variables
  * A Global variable is one that is defined outside a function, all scripts
- * and fucntions on the webpage can access it. I assume (probably incorrectly)
- * that this will mostly be used for 'querySelector' and not too much else
- * at this stange of development for me.
+ * and fucntions on the webpage can access it. Only works immediately for HTML that is not
+ * dynamically created
  */
 const nav_ul = document.getElementById("navbar__list");
 const trialData = document.getElementsByClassName("landing__container");
@@ -30,38 +29,52 @@ let activeSection = document.getElementById("section1");
 
 
  * Begin Main Functions
- *
+ 
  */
 
-//
-
+// creating li for navigation bar and setting Ids and classes
 for (let i = 1; i <= trialData.length; i++) {
     const entry = document.createElement("li");
-    entry.setAttribute("id", `bob${i}`);
-    let navId = document.getElementById(`bob${i}`);
-    let navActive = document.getElementById("bob1");
+    entry.setAttribute("id", `nav${i}`);
+    let navId = document.getElementById(`nav${i}`);
+
+//adding activeClassName to navigation "Section1" ONLY, so it starts highlighted
     if (i === 1) {
         entry.classList.add(activeClassName);
     }
 
+// finalizing the navigation bar, with scroll anchors
     const trialA = document.createElement("a");
     trialA.classList.add("menu__link");
     trialA.setAttribute("href", "#section" + i);
     trialA.innerText = `Section ${i}`;
     nav_ul.appendChild(entry);
     entry.appendChild(trialA);
+};
 
-    entry.addEventListener("click", function () {
-        const navId = document.getElementById(`bob${i}`);
+/*
+* the let navActive can only be set AFTER the navigation bar has been made, putting this 
+* before it's creation makes it "null" or "undefined" everytime. It also cannot go inside
+* the function below it as this will reset it to "nav1" everytime there is a click.  
+* Must also be let instead of const as this stops it from being changed 
+* when clicked "navActive = navId"   
+*/   
+let navActive = document.getElementById("nav1");
+
+/*
+* now the activeClassName can be changed for both the navigation and section when clicked
+* MAY ALSO WORK FOR SCROLL (SCROLL MAY MAKE THIS IRRELEVANT BUT AT LEAST I LEARNED IT)
+*/
+for (let i = 1; i <= trialData.length; i++) {
+const navId = document.getElementById(`nav${i}`);
+    navId.addEventListener("click", function () {
         navActive.classList.remove(activeClassName);
-        navId.classlist.add(activeClassName);
-        navActive = navId;
-    });
-
-    trialA.addEventListener("click", function () {
+        navId.classList.add(activeClassName);
+        navActive = navId;        
+        
         const section = document.getElementById(`section${i}`);
         activeSection.classList.remove(activeClassName);
         section.classList.add(activeClassName);
         activeSection = section;
     });
-}
+};

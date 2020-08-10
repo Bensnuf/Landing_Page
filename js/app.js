00eab8ad49853d39f64be060396dd109287edb63
Bensnuf <68095081+Bensnuf@users.noreply.github.com>
2020-08-10 11:48:29 -0500

    Works for everything except active change in Nav

--- js/app.js
+++ js/app.js
@@ -20,157 +20,48 @@
  * that this will mostly be used for 'querySelector' and not too much else
  * at this stange of development for me.
  */
-
-// const navlist = document.getElementsByClassName("navbar__menu").getElementById("navbar__list")
-// const navTrial = document.querySelector("ul");
-// const navClass = document.getElementsByClassName("header>nav>ul#navbar__list");
-// const dynamic = document.querySelectorAll(".navbar__menu");
-// const scroll = document.getElementById("section1");
-// const scroll2 = document.getElementById("section2");
-// const scroll3 = document.getElementById("section3");
-
-// const scrollPlease = document.querySelectorAll("h2");
-
-// const load = document.querySelector("div");
-
-// const trialData = document.getElementsByClassName("landing__container");
-
-// const dataArray = Array.from(document.querySelectorAll("[data-nav]"));
-// function dabby() {
-//     for (let i = 0; i < dataArray.length; i++) {
-//         dataArray[i];
-//     }
-// } //trying to create an array and use .shift to make new scroll to points
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-// document.addEventListener("click", function(){
-// alert("Hello World!");
-// });
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-//
 const nav_ul = document.getElementById("navbar__list");
 const trialData = document.getElementsByClassName("landing__container");
 const activeClassName = "your-active-class";
 let activeSection = document.getElementById("section1");
 
+/**
+ * End Global Variables
+
+
+ * Begin Main Functions
+ *
+ */
+
+//
+
 for (let i = 1; i <= trialData.length; i++) {
     const entry = document.createElement("li");
-    entry.classList.add("menu__link");
-    entry.innerText = `Section ${i}`;
-    nav_ul.appendChild(entry);
-    entry.addEventListener("click", function () {
-        const section = document.getElementById(`section${i}`);
-        section.scrollIntoView({ behavior: "smooth" });
+    entry.setAttribute("id", `bob${i}`);
+    let navId = document.getElementById(`bob${i}`);
+    let navActive = document.getElementById("bob1");
+    if (i === 1) {
+        entry.classList.add(activeClassName);
+    }
 
+    const trialA = document.createElement("a");
+    trialA.classList.add("menu__link");
+    trialA.setAttribute("href", "#section" + i);
+    trialA.innerText = `Section ${i}`;
+    nav_ul.appendChild(entry);
+    entry.appendChild(trialA);
+
+    entry.addEventListener("click", function () {
+        const navId = document.getElementById(`bob${i}`);
+        navActive.classList.remove(activeClassName);
+        navId.classlist.add(activeClassName);
+        navActive = navId;
+    });
+
+    trialA.addEventListener("click", function () {
+        const section = document.getElementById(`section${i}`);
         activeSection.classList.remove(activeClassName);
         section.classList.add(activeClassName);
         activeSection = section;
     });
 }
-
-// build the nav
-
-
-
-// for (let i = 1; i <= trialData.length; i++) {
-//     const nav_ul = document.getElementById("navbar__list");
-//     const entry = document.createElement("li");
-//     entry.classList.add("menu__link");
-
-//     const sectionLabel = "Section " + i;
-//     entry.innerText = sectionLabel
-
-//     nav_ul.appendChild(entry);
-//     entry.addEventListener("click", function () {
-//         // const dataArray = Array.from(document.querySelectorAll("[data-nav]"));
-//         // for (let i = 0; i < dataArray.length; i++) {
-//         //     dataArray[i];
-//         // }
-//         const section = document.querySelector(`#section${i}`)
-//         section.scrollIntoView()
-//     });
-// }
-
-// let bibbity = document.querySelector("#section1");
-// b
-
-// let Bob = function () {
-//     for (let i = 1; i <= trialData.length; i++) {
-//         const nav_ul = document.getElementById("navbar__list");
-//         const entry = document.createElement("li");
-//         const text = document.createTextNode("Section" + i)
-//         // const addAnchor = document.createElement("a");
-//         // text.classList.add("menu__link");
-//         text.classname = ("navbar__menu", "menu__link" );
-
-//         // const brainDead = document.getElementsByClassName("menu__link");
-//         // entry.innerText = "Section " + i;
-//         // const anchors = document.createElement("a");
-//         // anchors.setAttribute("class", "menu__link");
-//         nav_ul.appendChild(entry);
-//         entry.appendChild(text);
-//         // nav_ul.addEventListener("click", function () {
-//         //     anchors.scrollIntoView();
-//         };
-//     }
-// // };
-// Bob();
-
-// function create() {
-//     // for (let i = 1; i < ul.length; i++) {
-//     const navtoolbar = document.createElement("p");
-//     navtoolbar.innerText = "Section " + i;
-//     navtrial.appendChild(navtoolbar);
-//     // }
-// }
-
-// for (let i = 0; i < 3; i++) {
-//     create(i);
-// }
-
-// const navtoolbar = document.createElement("li");
-// navtoolbar.innerHTML = "Section 1";
-// navtrial.appendChild(navtoolbar);
-// navtoolbar.addEventListener("click", function () {
-//     scroll.scrollIntoView();
-// });
-
-// const navtoolbar2 = document.createElement("li");
-// navtoolbar2.innerHTML = "Section 2";
-// navtrial.appendChild(navtoolbar2);
-// navtoolbar2.addEventListener("click", function () {
-//     scroll2.scrollIntoView();
-// });
-
-// const navtoolbar3 = document.createElement("li");
-// navtoolbar3.innerHTML = "Section 3";
-// navtrial.appendChild(navtoolbar3);
-// navtoolbar3.addEventListener("click", function () {
-//     scroll3.scrollIntoView();
-// });
-
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
